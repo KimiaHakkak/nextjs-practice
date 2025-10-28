6aadf9047dc8554c0e63048d37f79fce298504a6
KimiaHakkak <kimiaghodsy@gmail.com>
2025-10-28 10:20:38 +0330

    next.config.mjs observation

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
+// ----------------------------------------------- what is this file -----------------------------------------------
+
+//Think of this file like the “settings panel” for your project. its the Configuration file for Next.js. pretty cool huh?
+
+
+
+
+
+/** @type {import('next').NextConfig} */          //That’s a JSDoc type annotation, it tells the code edditor what kind of object nextConfig is supposed to be. like: “Hey, this variable (nextConfig) should follow the shape of a NextConfig object from the Next.js library.”
+
 const nextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactCompiler: true,                            //it enables the React Compiler, which optimizes rendering and re-renders intelligently under the hood (kind of like an automatic performance boost).
 };
 
-export default nextConfig;
+export default nextConfig;                        //its a newer syntax than what was on the website. were using module system: ES Modules (next.config.mjs) instead of CommonJs (next.config.js)
+
