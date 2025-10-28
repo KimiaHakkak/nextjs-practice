e279c60ede70043e9c7b2f432e1b6908e2a99764
KimiaHakkak <kimiaghodsy@gmail.com>
2025-10-28 13:12:05 +0330

    page,js observed, its time to go for more routing files based on the website

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,3 +1,9 @@
+// ----------------------------------------------- what is this top level file -----------------------------------------------
+
+//The page file allows you to define UI that is unique to a route. You can create a page by default exporting a component from the file
+//A page file is required to make a route segment publicly accessible.
+//Pages are Server Components by default, but can be set to a Client Component.
+
 
 export default function Home() {
   return (
